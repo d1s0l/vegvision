1b35a61468fbd8068ff7e382eefc6538e96d66f8
d1s0l <d.solovyov214@gmail.com>
2026-05-10 15:26:47 +0300

    fix: paddig on auth pages

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,41 +1,40 @@
-import Image from "next/image"
-import styles from "@/shared/ui/auth-shell/AuthShell.module.scss"
+import Image from "next/image";
 import Link from "next/link";
+import { Metadata } from "next";
+import styles from "@/shared/ui/auth-shell/AuthShell.module.scss";
 import { ButtonBack } from "@/shared/ui/button-back";
 import { LoginForm } from "@/features/auth/login-form";
-import { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: "Авторизация"
-}
+  title: "Авторизация",
+};
 
 export default function Login() {
-    return(
-        <div className={styles.loginMenu}>
-            <div className={styles.menu}>
-                <ButtonBack/>
-                <div className={styles.auth}>
-                    <Link href="/" className={styles.logo}>
-                        <Image 
-                            src="./logo/logo_auth.svg"
-                            alt="Logo"
-                            width={79}
-                            height={65}
-                        />
-                        <h1>VegVision</h1>
-                    </Link>
-                    <LoginForm />
-                    <div className={styles.regist}>
-                        <span>Нет аккаунта?</span>
-                        <a href="/registration">Регистрация</a>
-                    </div>
-                </div>
-            </div>
-            <div className={styles.wrapper}>
-                <div className={styles.container}>
-
-                </div>
-            </div>
+  return (
+    <div className={styles.loginMenu}>
+      <div className={styles.menu}>
+        <ButtonBack />
+        <div className={styles.auth}>
+          <Link href="/" className={styles.logo}>
+            <Image
+              src="./logo/logo_auth.svg"
+              alt="Logo"
+              width={79}
+              height={65}
+            />
+            <h1>VegVision</h1>
+          </Link>
+          <LoginForm />
+          <div className={styles.regist}>
+            <span>Нет аккаунта?</span>
+            <Link href="/registration">Регистрация</Link>
+          </div>
         </div>
-    )
+      </div>
+
+      <div className={styles.wrapper}>
+        <div className={styles.container} />
+      </div>
+    </div>
+  );
 }
--- app/(auth)/registration/page.tsx
+++ app/(auth)/registration/page.tsx
@@ -1,40 +1,40 @@
-import Image from "next/image"
-import styles from "@/shared/ui/auth-shell/AuthShell.module.scss"
+import Image from "next/image";
 import Link from "next/link";
+import { Metadata } from "next";
+import styles from "@/shared/ui/auth-shell/AuthShell.module.scss";
 import { ButtonBack } from "@/shared/ui/button-back";
 import { RegistrationForm } from "@/features/auth/registration-form";
-import { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: "Регистрация"
-}
-
+  title: "Регистрация",
+};
 
 export default function Registraion() {
-    return(
-        <div className={styles.loginMenu}>
-            <ButtonBack />
-            <div className={styles.auth}>
-                <Link href="/" className={styles.logo}>
-                    <Image 
-                        src="./logo/logo_auth.svg"
-                        alt="Logo"
-                        width={79}
-                        height={65}
-                    />
-                    <h1>VegVision</h1>
-                </Link>
-                <RegistrationForm />
-                <div className={styles.regist}>
-                    <span>Нет аккаунта?</span>
-                    <a href="/login">Войти</a>
-                </div>
-            </div>
-            <div className={styles.wrapper}>
-                <div className={styles.container}>
-
-                </div>
-            </div>
+  return (
+    <div className={styles.loginMenu}>
+      <div className={styles.menu}>
+        <ButtonBack />
+        <div className={styles.auth}>
+          <Link href="/" className={styles.logo}>
+            <Image
+              src="./logo/logo_auth.svg"
+              alt="Logo"
+              width={79}
+              height={65}
+            />
+            <h1>VegVision</h1>
+          </Link>
+          <RegistrationForm />
+          <div className={styles.regist}>
+            <span>Уже есть аккаунт?</span>
+            <Link href="/login">Войти</Link>
+          </div>
         </div>
-    )
+      </div>
+
+      <div className={styles.wrapper}>
+        <div className={styles.container} />
+      </div>
+    </div>
+  );
 }
